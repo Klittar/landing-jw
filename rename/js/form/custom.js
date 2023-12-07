4bb0cf9461f39f9989378df640b98893cdb79f5f
Klittar <100770588+Klittar@users.noreply.github.com>
2023-12-07 21:32:37 +0300

    Delete rename directory

--- rename/js/form/custom.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.custom-file-input').addEventListener('change',function(e){
-    var fileName = document.getElementById("user_project").files[0].name;
-    var nextSibling = e.target.nextElementSibling
-    nextSibling.innerText = fileName
-});
